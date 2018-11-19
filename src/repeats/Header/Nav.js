991e843d9beb29f1cc70f7dd22e3ddd59408ae6f
Reid South <rbsouth@gmail.com>
2018-11-18 21:23:58 -0700

    Navlinks color and font-size

--- src/repeats/Header/Nav.js
+++ src/repeats/Header/Nav.js
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
 const HeaderNav = () => {
   return (
     <nav className="nav">
-      <Link className="navLink" to="/">Home</Link> <Link to="/about">About</Link>{" "}
+      <Link className="navLink" to="/">Home</Link>
+      <Link className="navLink" to="/about">About</Link>{" "}
       <Link className="navLink" to="/contact">Contact</Link>{" "}
       <Link className="navLink" to="/services">Towing Services</Link>
     </nav>
