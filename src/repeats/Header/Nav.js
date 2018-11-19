575201f1639d932bbef4d6ce3e4bd61896375ac2
Reid South <rbsouth@gmail.com>
2018-11-18 22:17:11 -0700

    info above nav added

--- src/repeats/Header/Nav.js
+++ src/repeats/Header/Nav.js
@@ -4,15 +4,22 @@ import { Link } from "react-router-dom";
 
 const HeaderNav = () => {
   return (
-    <nav className="nav">
-      <Link className="navLink" to="/">Home</Link>
-      <div className="navBreak"></div>
-      <Link className="navLink innerNav" to="/about">About</Link>{" "}
-      <div className="navBreak"></div>
-      <Link className="navLink innerNav" to="/contact">Contact</Link>{" "}
-      <div className="navBreak"></div>
-      <Link className="navLink" to="/services">Towing Services</Link>
-    </nav>
+    <navcont>
+      <info className="info">
+        <div className="infoLeft"></div>
+        <img className="logo" src="" alt="United Truck Service Logo"></img>
+        <div className="infoRight"></div>
+      </info>
+      <nav className="nav">
+        <Link className="navLink" to="/">HOME</Link>
+        <div className="navBreak"></div>
+        <Link className="navLink innerNav" to="/about">ABOUT</Link>{" "}
+        <div className="navBreak"></div>
+        <Link className="navLink innerNav" to="/contact">CONTACT</Link>{" "}
+        <div className="navBreak"></div>
+        <Link className="navLink" to="/services">TOWING SERVICES</Link>
+      </nav>
+    </navcont>
   );
 };
 
