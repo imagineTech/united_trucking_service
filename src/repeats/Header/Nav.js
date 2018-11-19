06753889ed5ce318aab5b555060c4e11f80411f1
Reid South <rbsouth@gmail.com>
2018-11-18 19:58:58 -0700

    nav style imported

--- src/repeats/Header/Nav.js
+++ src/repeats/Header/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Header.css"
 import { Link } from "react-router-dom";
 
 const HeaderNav = () => {
   return (
-    <nav>
+    <nav className="nav">
       <Link to="/">Home</Link> <Link to="/about">About</Link>{" "}
       <Link to="/contact">Contact</Link>{" "}
       <Link to="/services">Towing Services</Link>
