61279e8868e9968315428de2ddb1eec165a48718
Reid South <rbsouth@gmail.com>
2018-11-18 21:42:42 -0700

    Dividers added to header

--- src/repeats/Header/Nav.js
+++ src/repeats/Header/Nav.js
@@ -6,8 +6,11 @@ const HeaderNav = () => {
   return (
     <nav className="nav">
       <Link className="navLink" to="/">Home</Link>
+      <div className="navBreak"></div>
       <Link className="navLink innerNav" to="/about">About</Link>{" "}
+      <div className="navBreak"></div>
       <Link className="navLink innerNav" to="/contact">Contact</Link>{" "}
+      <div className="navBreak"></div>
       <Link className="navLink" to="/services">Towing Services</Link>
     </nav>
   );
